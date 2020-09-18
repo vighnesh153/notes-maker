6821c6162c471f435296294562e9c856980a9253
vighnesh153 <vighnesh.raut13@gmail.com>
2020-09-18 17:50:09 +0530

    setup basic server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "all works!"
+  });
+});
+
+app.listen(3042, () => {
+  console.log(`
+      ################################################
+      🛡️  Server listening on port: ${3042} 🛡️ 
+      ################################################
+    `);
+});
